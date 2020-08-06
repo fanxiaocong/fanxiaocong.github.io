971086af83dbf31489b882f2b01dcd4de9a94a26
@fanxiaocong <1016697223@qq.com>
2020-08-06 17:51:59 +0800

    Site updated: 2020-08-06 17:51:58

--- js/diaspora.js
+++ js/diaspora.js
@@ -502,5 +502,4 @@ $(function() {
     if (comment.data('ae') == true){
         comment.click();
     }
-    console.log("%c Github %c","background:#24272A; color:#ffffff","","https://github.com/Fechin/hexo-theme-diaspora")
 })
